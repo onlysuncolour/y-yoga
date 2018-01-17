7ea27597043908f6f9436b3289cca34d3e23b63c
Young Liu <Young@Young.local>
2018-01-17 15:45:48 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 var less = require("less");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+ 
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
