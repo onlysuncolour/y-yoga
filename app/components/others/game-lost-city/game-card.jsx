a95e8e19f77da4082e14cd2a5302278bdcaaad7d
young <onlysuncolour@sina.com>
2018-02-16 07:43:03 +0800

    失落之城 - 页面结构

---
+++ app/components/others/game-lost-city/game-card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class GameCard extends React.Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="lost-city-card">
+        卡牌
+      </div>
+    )
+  }
+}
+
+module.exports = {GameCard}
--- app/components/others/game-lost-city/game-playing-page.jsx
+++ app/components/others/game-lost-city/game-playing-page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {GameCard} from './game-card'
 class GamePlayingPage extends React.Component{
   constructor() {
     super();
@@ -10,6 +11,63 @@ class GamePlayingPage extends React.Component{
     return (
       <div className="lost-city-game-playing-page">
         lost-city-game-playing
+        <div className="game-info">
+          <div className="game-title">
+            失落之城
+          </div>
+          <div className="players">
+            abc VS edf
+          </div>
+          <div className="buttons">
+            查看规则
+            退出
+            认输
+          </div>
+        </div>
+        <div className="enemy-hand">
+          <GameCard back="true"></GameCard>
+          <GameCard back="true"></GameCard>
+          <GameCard back="true"></GameCard>
+          <GameCard back="true"></GameCard>
+          <GameCard back="true"></GameCard>
+          <GameCard back="true"></GameCard>
+          <GameCard back="true"></GameCard>
+          <GameCard back="true"></GameCard>
+        </div>
+        <div className="enemy-pile">
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+        </div>
+        <div className="discard-pile">
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+        </div>
+        <div className="pile">
+          <GameCard ></GameCard>
+        </div>
+        <div className="my-pile">
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+          <GameCard ></GameCard>
+        </div>
+        <div className="my-hand">
+          <GameCard operate></GameCard>
+          <GameCard operate></GameCard>
+          <GameCard operate></GameCard>
+          <GameCard operate></GameCard>
+          <GameCard operate></GameCard>
+          <GameCard operate></GameCard>
+          <GameCard operate></GameCard>
+          <GameCard operate></GameCard>
+        </div>
       </div>
     )
   }
