ce9f0f7d786bb7cec5ed1faec1d7f3d494f157ec
young <onlysuncolour@sina.com>
2018-02-21 16:29:26 +0800

    添加markdown预览.....

--- app/components/blog/blog-edit.jsx
+++ app/components/blog/blog-edit.jsx
@@ -6,13 +6,29 @@ import './blog-edit.less'
 class BlogEdit extends React.Component {
   constructor() {
     super();
+    this.state = {
+      blogContent: '# This is a header\n\nAnd this is a paragraph',
+      blogTitle: "123",
+    }
+    this.handleBlogChange = this.handleBlogChange.bind(this)
+  }
+  handleBlogChange(event) {
+    let newState = {};
+    newState[event.target.name] = event.target.value
+    this.setState(newState);
   }
   componentDidMount () {}
   render () {
-    const input = '# This is a header\n\nAnd this is a paragraph'
+    // const input = '# This is a header\n\nAnd this is a paragraph'
     return (
       <div className="blog-edit-page">
-        <Markdown source={input} />
+        <div className="blog">
+          <input type="text" value={this.state.title} name="blogTitle" onChange={this.handleBlogChange} />
+          <textarea value={this.state.blogContent} name="blogContent" onChange={this.handleBlogChange} />
+        </div>
+        <div className="preview">
+          <Markdown source={this.state.blogContent} />
+        </div>
       </div>
     )
   }
