bddca753891cf0802432d8ed93dc7e1cb1a7f61f
young <onlysuncolour@sina.com>
2018-02-22 21:32:04 +0800

    一丢丢edit的样式

--- app/components/blog/blog-edit.jsx
+++ app/components/blog/blog-edit.jsx
@@ -22,12 +22,19 @@ class BlogEdit extends React.Component {
     // const input = '# This is a header\n\nAnd this is a paragraph'
     return (
       <div className="blog-edit-page">
-        <div className="blog">
-          <input type="text" value={this.state.title} name="blogTitle" onChange={this.handleBlogChange} />
-          <textarea value={this.state.blogContent} name="blogContent" onChange={this.handleBlogChange} />
+        <div className="blog-title">
+          <input type="text" value={this.state.title} name="blogTitle" onChange={this.handleBlogChange} placeholder="博文标题" />
         </div>
-        <div className="preview">
-          <Markdown source={this.state.blogContent} />
+        <div className="blog">
+          <div className="content">
+            <div className="content-config">
+              123
+            </div>
+            <textarea value={this.state.blogContent} name="blogContent" onChange={this.handleBlogChange} />
+          </div>
+          <div className="preview">
+            <Markdown source={this.state.blogContent} />
+          </div>
         </div>
       </div>
     )
