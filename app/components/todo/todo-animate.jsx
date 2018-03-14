f590a215a1c8ee85181d0316d5481f77b76f6e82
young <onlysuncolour@sina.com>
2018-03-14 10:03:48 +0800

    feat: 跑马灯II

--- app/components/todo/todo-animate.jsx
+++ app/components/todo/todo-animate.jsx
@@ -4,46 +4,76 @@ require('./todo-animate.less')
 class TodoAnimate extends React.Component{
   constructor() {
     super();
+    this.state = {
+      lines: [{
+        name: "line1", speed: 80, spans: [
+        "Javascript",
+        "nodeJs",
+        "socket.io",
+        "React",
+        "VUE.js",
+        "Angular",
+        "Express",
+        "ECMAScript",
+        "Webpack",
+        "GraphQL",
+      ]}, {
+        name: "line2", speed: 120, spans: [
+        "hello world",
+        "var",
+        "Async/Await",
+        "let",
+        "callback",
+        "console.log",
+        "debugger",
+        "const",
+        "function",
+      ]}, {
+        name: "line3", speed: 100, spans: [
+        "FREE THINKING",
+        "FSOCIETY",
+        "HACKER",
+        "THIS IS US",
+        "YOUNG",
+        "KEEP FOOLISH",
+        "DESIGN",
+        "BLOCKCHAIN"
+      ]}]
+    }
   }
   componentDidMount() {
 
   }
   componentDidUpdate() {
-    let speed = 200;
-    let width = this.refs.content.clientWidth
-    // this.refs.content.width = width
-    width = width / 2
-    var onceTime = width / speed * 1000; //ms
-    this.refs.content.style.animationDuration = onceTime + "ms"
+    this.state.lines.forEach(line => {
+      let speed = line.speed;
+      let width = this.refs[line.name].clientWidth
+      width = width / 2
+      var onceTime = width / speed * 1000; //ms
+      this.refs[line.name].style.animationDuration = onceTime + "ms"
+    })
   }
   render() {
     return (
       <div className="todo-animate-tab">
-        <div className="line-one">
-          <span className="content" ref="content">
-            <span>Javascript</span>
-            <span>nodeJs</span>
-            <span>socket.io</span>
-            <span>React</span>
-            <span>VUE.js</span>
-            <span>Angular</span>
-            <span>Express</span>
-            <span>ECMAScript</span>
-            <span>Webpack</span>
-            <span>GraphQL</span>
-
-            <span>Javascript</span>
-            <span>nodeJs</span>
-            <span>socket.io</span>
-            <span>React</span>
-            <span>VUE.js</span>
-            <span>Angular</span>
-            <span>Express</span>
-            <span>ECMAScript</span>
-            <span>Webpack</span>
-            <span>GraphQL</span>
-          </span>
-        </div>
+        {
+          this.state.lines.map((line, index) => (
+            <div className="line" key={line.name}>
+              <span className={`content ${index%2==0? "fade-in-left" : "fade-in-right"}`} ref={line.name} >
+                {
+                  line.spans.map(span => (
+                    <span key={span}>{span}</span>
+                  ))
+                }
+                {
+                  line.spans.map(span => (
+                    <span key={span}>{span}</span>
+                  ))
+                }
+              </span>
+            </div>
+          ))
+        }
       </div>
     )
   }
