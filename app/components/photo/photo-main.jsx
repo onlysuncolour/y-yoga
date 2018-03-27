9b3f1b84dd7f9c66459517a417585023e2ad4191
liangyj021 <617062781@qq.com>
2018-03-27 09:01:32 +0800

    feat: photo

--- app/components/photo/photo-main.jsx
+++ app/components/photo/photo-main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './photo-main.less'
 
 class PhotoPage extends React.Component{
   constructor() {
@@ -9,7 +10,15 @@ class PhotoPage extends React.Component{
   render() {
     return (
       <div className="photo-main-page">
-        photo-page
+        <div className="container">
+          <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="330" height="86" src="//music.163.com/outchain/player?type=2&id=28566360&auto=1&height=66"></iframe>
+          <figure>
+            <p>黄浦江上的的卢浦大桥</p>
+            <p>拍摄者：W3School 项目组，拍摄时间：2010 年 10 月</p>
+            <img src="../../images/small/8.jpg" width="350" height="234" />
+          </figure>
+          {/* <audio src="song.ogg" controls="controls"></audio> */}
+        </div>
       </div>
     )
   }
