c308f1242f3340d42112e004cec051ad8de6ab07
young <onlysuncolour@sina.com>
2018-08-22 08:20:10 +0800

    fix: less loader option

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,8 @@ module.exports = {
         }, {
           loader: "css-loader"
         }, {
-          loader: "less-loader"
+          loader: "less-loader",
+          options: { javascriptEnabled: true }
         }]
       }, { test: /\.css$/,
         use: [{
